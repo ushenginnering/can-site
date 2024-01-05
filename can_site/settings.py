7747b392fa8d15bd2e1033319596c0fd069939f4
winning godspower <winninggodspower@gmail.com>
2024-01-05 11:18:49 +0100

    fix: fixed the allowed host url

--- can_site/settings.py
+++ can_site/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = ['ushengineering.com', 'www.ushengineering.com', 'localhost']
+ALLOWED_HOSTS = ['churcharisenetwork.com.ng', 'churcharisenetwork.com.ng', 'localhost']
 
 
 # Application definition
