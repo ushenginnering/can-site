ea3aae664bf825847ed4eda16be70402d3ef4688
winning godspower <winninggodspower@gmail.com>
2024-01-15 13:23:24 +0100

    feat: add media url to django urls

--- can_site/urls.py
+++ can_site/urls.py
@@ -1,9 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("", include('main.urls')),
     path("", include('user_authentication.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
