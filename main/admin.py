9c56a5c060249ebc604451fcea3cbb78e0b3d264
winning godspower <winninggodspower@gmail.com>
2024-01-16 12:04:41 +0100

    feat: add givng model to admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Announcement
-from .models import Gallery
+from .models import Announcement, Gallery, Giving
 
 # Register your models here.
 admin.site.register(Announcement)
 admin.site.register(Gallery)
+admin.site.register(Giving)
