220e7740a90e5a4fb4a6b6ceca6ee76bdd22514f
winning godspower <winninggodspower@gmail.com>
2024-01-18 10:55:37 +0100

    modify: modify css in pages

--- main/views.py
+++ main/views.py
@@ -26,7 +26,7 @@ def gallery_view(requests):
     return render(requests, 'gallery.html', context)
 
 def partner_view(requests):
-    return render(requests, 'partner.html')
+    return render(requests, 'partners.html')
 
 def publication_view(requests):
     return render(requests, 'publications.html')
