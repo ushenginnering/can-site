d69709e7d4622211938b59bedfc59e4be1dcc346
winning godspower <winninggodspower@gmail.com>
2024-01-18 12:28:33 +0100

    feat: add meeting and welfare models to admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Announcement, Gallery, Giving
+from .models import Announcement, Gallery, Giving, MeetingReport, Welfare
 
 # Register your models here.
 admin.site.register(Announcement)
 admin.site.register(Gallery)
 admin.site.register(Giving)
+admin.site.register(MeetingReport)
+admin.site.register(Welfare)
